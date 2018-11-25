fbdec992200badca11c32a43a8c76f003a0b8e0b
Michael-Kolbasov <trololoshenkilolo@yandex.ru>
2018-11-25 13:16:49 +0400

    Reworked the hashCode() for Element.

--- src/main/java/game/objects/Element.java
+++ src/main/java/game/objects/Element.java
@@ -23,9 +23,14 @@ public class Element {
         this.x = x;
     }
 
+    /**
+     * Each cell has a unique combination of Y and X coordinate.
+     * The multiplying (y + 1) block serves the purpose of distinguishing cells like (0, 1) and (1, 0).
+     * @return  hashcode of a cell
+     */
     @Override
     public int hashCode() {
-        return x + y;
+        return (x + y) * (y + 1);
     }
 
     @Override
